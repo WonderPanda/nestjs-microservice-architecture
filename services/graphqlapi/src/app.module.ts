77ad922aaa557aa1dbd2403a5a9c9f1a8cc8c840
Jesse Carter <jesse.r.carter@gmail.com>
2018-09-12 18:15:09 -0400

    GQL setup with docker

--- services/graphqlapi/src/app.module.ts
+++ services/graphqlapi/src/app.module.ts
@@ -5,17 +5,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatalogModule } from './catalog/catalog.module';
 
+// Only generate the TS interfaces when not running in production
+const env = process.env.GRAPHQL_ENV;
+
+const gqlModule =
+  env === 'production'
+    ? GraphQLModule.forRoot({
+        typePaths: ['./**/*.gql']
+      })
+    : GraphQLModule.forRoot({
+        typePaths: ['./**/*.gql'],
+        definitions: {
+          path: join(process.cwd(), 'src/schemas/graphql.d.ts')
+        }
+      });
+
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.gql'],
-      definitions: {
-        path: join(process.cwd(), 'src/SDL/graphql.d.ts'),
-      },
-    }),
-    CatalogModule,
-  ],
+  imports: [gqlModule, CatalogModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
